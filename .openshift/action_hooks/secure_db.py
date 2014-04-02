b01fca08942c23395db0d08be2449084fade437c
Sam J <sam.jiang@karoshealth.com>
2014-04-01 19:19:27 -0700

    renamed openshift in action_hooks

--- .openshift/action_hooks/secure_db.py
+++ .openshift/action_hooks/secure_db.py
@@ -2,7 +2,7 @@
 import hashlib, imp, os, sqlite3
 
 # Load the OpenShift helper library
-lib_path      = os.environ['OPENSHIFT_REPO_DIR'] + 'wsgi/openshift/'
+lib_path      = os.environ['OPENSHIFT_REPO_DIR'] + 'wsgi/ghungry/'
 modinfo       = imp.find_module('openshiftlibs', [lib_path])
 openshiftlibs = imp.load_module('openshiftlibs', modinfo[0], modinfo[1], modinfo[2])
 
